cd57e950ba77d6266364f74b4aa871756cb474fd
AnthonyMChong <Anthonymchong@gmail.com>
2019-03-26 21:16:27 -0700

    completed the conditional module from day2 of 10 days of JS

--- day2-10daysofJS/day2.js
+++ day2-10daysofJS/day2.js
@@ -28,6 +28,31 @@ function vowelsAndConsonants(s) {
     // console.log(ordereds)
 }
 
+function getGrade(score) {
+    let grade;
+    // Write your code here
+    if (score > 25) {
+        grade = "A";
+    }
+    else if (score > 20) {
+        grade = "B";
+    }
+    else if (score > 15) {
+        grade = "C"
+    }
+    else if (score > 10) {
+        grade = "D"
+    }
+    else if (score > 5) {
+        grade = "E"
+    }
+    else {
+        grade = "F"
+    }
+    return grade;
+}
+
 
 // console.log(vowelsAndConsonants("javascriptloops"));
 vowelsAndConsonants("javascriptloops")
+console.log(getGrade(7));
