516eebc4bcf88de63c0ed4e8010fac7820e442b4
AnthonyMChong <Anthonymchong@gmail.com>
2019-03-26 20:57:06 -0700

    missed a day 1 training. finished it now

---
+++ letandconst/letandconst.js
@@ -0,0 +1,12 @@
+function main() {
+    const PI = Math.PI
+    // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+    let lineinput = 2.6
+    // Print the area of the circle:
+    console.log((lineinput**2)* PI);
+    
+    // Print the perimeter of the circle:
+    console.log(lineinput*PI*2);
+}
+
+main()
