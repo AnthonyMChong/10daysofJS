0a1510445d185b2ada4f9a1c5e7d9f40b8b5ddff
AnthonyMChong <Anthonymchong@gmail.com>
2019-03-28 22:20:02 -0700

    continueing day 3 of JS with reverse string try catch

--- day3-10daysofJS/day3.js
+++ day3-10daysofJS/day3.js
@@ -14,5 +14,23 @@ function getSecondLargest(nums) {
     return secondlargest
 }
 
+function reverseString(s) {
+    try {
+        let revs;
+        revs = s.split("").reverse().join("");
+        console.log(revs);
+    } catch (e) {
+        console.log(e.message);
+        console.log(s);
+    }
+    
+}
+
+
+
+
 var a = [2, 3, 6, 6, 5];
 console.log(getSecondLargest(a));
+var s = "reverseme"
+reverseString(s)
+reverseString(1234)
