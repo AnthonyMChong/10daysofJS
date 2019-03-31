ceaaa035bc9e8eb9cb64470682488697e1bfdb96
AnthonyMChong <Anthonymchong@gmail.com>
2019-03-30 22:49:01 -0700

    finished day 4 of 10 days of JS

--- day4-10daysofJS/day4.js
+++ day4-10daysofJS/day4.js
@@ -7,4 +7,34 @@ function Rectangle(a, b) {
     return rectInfo
 }
 
+
+function getCount(objects) {
+    var equalObjects = 0;
+    for (let o = 0; o < objects.length ; o++){
+        if (objects[o].x == objects[o].y) {
+            equalObjects++;
+        }
+    }
+    return equalObjects
+}
+
+function Polygon(dimensions) {
+    this.perimeter = function () {
+        let p = 0
+        for (let o = 0; o < dimensions.length; o++) {
+            p += dimensions[o]
+        }
+        return p
+    }
+}
+
+
+const rectangle = new Polygon([10, 20, 10, 20]);
+const square = new Polygon([10, 10, 10, 10]);
+const pentagon = new Polygon([10, 20, 30, 40, 43]);
+
+console.log(rectangle.perimeter());
+console.log(square.perimeter());
+console.log(pentagon.perimeter());
 console.log(Rectangle(4,5))
+
