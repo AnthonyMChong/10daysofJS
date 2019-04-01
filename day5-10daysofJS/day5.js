9650460db1a55e6fb1ac893c681e435c2e984ee4
AnthonyMChong <Anthonymchong@gmail.com>
2019-04-01 01:26:54 -0700

    attempting to learn literals... need to fix current bug

--- day5-10daysofJS/day5.js
+++ day5-10daysofJS/day5.js
@@ -18,6 +18,18 @@ class Square extends Rectangle{
     }
 }
 
+function sides(literals, ...expressions) {
+    var exparray = [expressions[0], expressions[1]]
+    var area = expressions[0] * expressions[1]
+    for (let x = 0; x < exparray.length; x++){
+        console.log((expressions[x] + Math.sqrt(expressions[x] ** 2 - 16 * area))/4)
+        expressions[x] = (expressions[x] + Math.sqrt(expressions[x] ** 2 - 16 * area))/4
+    }
+    console.log(expressions[0])
+    return (exparray.sort());
+}
+
+
 /*
  * Create a Square class that inherits from Rectangle and implement its class constructor
  */
@@ -26,6 +38,11 @@ const rec = new Rectangle(3, 4);
 const sqr = new Square(3);
 
 console.log(rec.area());
+console.log(sqr.area());
+
+let s1 = 10;
+let s2 = 14;
+const [x, y] = sides`The area is: ${s1 * s2}.\nThe perimeter is: ${2 * (s1 + s2)}.`;
 console.log(sqr.area());
 
 
