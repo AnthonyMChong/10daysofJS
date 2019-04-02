623482769698d4ea8abb00abeff477ef9c486a61
AnthonyMChong <Anthonymchong@gmail.com>
2019-04-01 20:31:19 -0700

    finished learning about literals. the bug was due to a misunderstanding of the function inputs

--- day5-10daysofJS/day5.js
+++ day5-10daysofJS/day5.js
@@ -20,12 +20,14 @@ class Square extends Rectangle{
 
 function sides(literals, ...expressions) {
     var exparray = [expressions[0], expressions[1]]
-    var area = expressions[0] * expressions[1]
-    for (let x = 0; x < exparray.length; x++){
-        console.log((expressions[x] + Math.sqrt(expressions[x] ** 2 - 16 * area))/4)
-        expressions[x] = (expressions[x] + Math.sqrt(expressions[x] ** 2 - 16 * area))/4
-    }
-    console.log(expressions[0])
+    var area = expressions[0]
+    var perimeter =   expressions[1]
+    // console.log(exparray)
+    // console.log(area , perimeter)
+    exparray[0] = (perimeter + Math.sqrt(((perimeter) ** 2) - 16 * area))/4
+    exparray[1] = (perimeter - Math.sqrt(((perimeter) ** 2) - 16 * area))/4
+    // console.log((expressions[x] + Math.sqrt(expressions[x] ** 2 - 16 * area))/4)
+    // console.log(exparray[x])
     return (exparray.sort());
 }
 
@@ -42,8 +44,9 @@ console.log(sqr.area());
 
 let s1 = 10;
 let s2 = 14;
-const [x, y] = sides`The area is: ${s1 * s2}.\nThe perimeter is: ${2 * (s1 + s2)}.`;
 console.log(sqr.area());
+const [x, y] = sides`The area is: ${s1 * s2}.\nThe perimeter is: ${2 * (s1 + s2)}.`;
+console.log([x,y])
 
 
 // if (JSON.stringify(Object.getOwnPropertyNames(Square.prototype)) === JSON.stringify([ 'constructor' ])) {
