865f887075bd8d026651af43856f89bf2cbea255
Anthony Chong <anthonymchong@gmail.com>
2019-04-05 22:10:13 -0700

    continue the regex learnign with more regular expressions in 10 days of JS

--- day7-10daysofJS/day7.js
+++ day7-10daysofJS/day7.js
@@ -15,15 +15,48 @@ function regexVar() {
 }
 
 
-const re = regexVar();
-const s = "aaadsfaaa"
-const s2 = "eaadsfaaaa"
-const s3 = "uaadsfaaa"
-const s4 = "sfytfe"
-const s5 = "esfytfe"
+function regexTitle() {
+  /*
+   * Declare a RegExp object variable named 're'
+   * It must match a string that starts and ends with the same vowel (i.e., {a, e, i, o, u})
+   */
+  
+ var re = /^(Dr|Mr|Mrs|Ms|Er)\.[a-zA-Z]+$/
 
-console.log(re.test(s));
-console.log(re.test(s2));
-console.log(re.test(s3));
-console.log(re.test(s4));
-console.log(re.test(s5));
+  /*
+   * Do not remove the return statement
+   */
+  return re;
+}
+
+const titlere = regexTitle()
+
+// const re = regexVar();
+// const s = "aaadsfaaa"
+// const s2 = "eaadsfaaaa"
+// const s3 = "uaadsfaaa"
+// const s4 = "sfytfe"
+// const s5 = "esfytfe"
+const title = "Mranthony"
+const title1 = "Ms.Chong"
+const title2 = "Mrs.Allison"
+const title3 = "M.Bison"
+const title4 = "Bison"
+const title5 = "DooDoo"
+const title6 = "rude"
+const title7 = "MrsAllison"
+
+// console.log(re.test(s));
+// console.log(re.test(s2));
+// console.log(re.test(s3));
+// console.log(re.test(s4));
+// console.log(re.test(s5));
+
+console.log(titlere.test(title))
+console.log(titlere.test(title1))
+console.log(titlere.test(title2))
+console.log(titlere.test(title3))
+console.log(titlere.test(title4))
+console.log(titlere.test(title5))
+console.log(titlere.test(title6))
+console.log(titlere.test(title7))
