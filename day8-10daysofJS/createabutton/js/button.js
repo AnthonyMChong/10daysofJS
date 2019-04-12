58f4d442f9d9a853bb04c9b994bfac6ab8c34286
AnthonyMChong <Anthonymchong@gmail.com>
2019-04-12 00:43:08 -0700

    gave button onclick functionality to satisfy day 8 challenge for 10 days of JS

--- day8-10daysofJS/createabutton/js/button.js
+++ day8-10daysofJS/createabutton/js/button.js
@@ -1,20 +1,9 @@
-<p>This is what our code produces:</p>
-<button id='htmlButton1' class='button'>I'm an HTML button!</button>
-<button id='htmlButton2' class='button'>I'm an HTML button!</button>
+// getting reference of the button from the html document
+var htmlButton = document.getElementById('htmlNumButton');
 
-<script>
-    /* Create a button element */
-    var clickMeButton = document.createElement('button');
-    /* Set the button's text label */
-    clickMeButton.innerHTML = 'I\'m a JavaScript button!';
-    /* Set the button's id */
-    clickMeButton.id = 'jsButton';
-    /* Set the button's style class */
-    clickMeButton.className = 'button';
-    /* Add the button to the page */
-    document.body.appendChild(clickMeButton);
-    
-    /* Get the element with id='htmlButton2' */
-    var htmlButton = document.getElementById('htmlButton2');
-    /* Modify the text label for htmlButton2 */
-    htmlButton.innerHTML = 'I\'m a modified HTML button!';
+var buttonValue = 0;
+
+htmlButton.onclick = function() {
+    buttonValue ++;
+    htmlButton.innerHTML = buttonValue.toString();
+}
